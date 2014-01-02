b0f30ff62ece908cc1bdf6c57599e6a367bb40fd
David Faure <faure@kde.org>
2014-01-02 01:10:55 +0100

    Split out one class per header, for the benefit of forwarding headers.
    Too lazy to split up the .cpp files too - can always be done later.

--- src/kzip.h
+++ src/kzip.h
@@ -20,6 +20,8 @@
 
 #include <karchive.h>
 
+#include "kzipfileentry.h" // for source compat
+
 class KZipFileEntry;
 /**
  *   A class for reading / writing zip archives.
@@ -159,62 +161,6 @@ protected:
 private:
     class KZipPrivate;
     KZipPrivate *const d;
-};
-
-/**
- * A KZipFileEntry represents an file in a zip archive.
- */
-class KARCHIVE_EXPORT KZipFileEntry : public KArchiveFile
-{
-public:
-    /**
-     * Creates a new zip file entry. Do not call this, KZip takes care of it.
-     */
-    KZipFileEntry(KZip *zip, const QString &name, int access, const QDateTime &date,
-                  const QString &user, const QString &group, const QString &symlink,
-                  const QString &path, qint64 start, qint64 uncompressedSize,
-                  int encoding, qint64 compressedSize);
-
-    /**
-     * Destructor. Do not call this.
-     */
-    ~KZipFileEntry();
-
-    int encoding() const;
-    qint64 compressedSize() const;
-
-    /// Only used when writing
-    void setCompressedSize(qint64 compressedSize);
-
-    /// Header start: only used when writing
-    void setHeaderStart(qint64 headerstart);
-    qint64 headerStart() const;
-
-    /// CRC: only used when writing
-    unsigned long crc32() const;
-    void setCRC32(unsigned long crc32);
-
-    /// Name with complete path - KArchiveFile::name() is the filename only (no path)
-    const QString &path() const;
-
-    /**
-     * @return the content of this file.
-     * Call data() with care (only once per file), this data isn't cached.
-     */
-    QByteArray data() const Q_DECL_OVERRIDE;
-
-    /**
-     * This method returns a QIODevice to read the file contents.
-     * This is obviously for reading only.
-     * Note that the ownership of the device is being transferred to the caller,
-     * who will have to delete it.
-     * The returned device auto-opens (in readonly mode), no need to open it.
-     */
-    QIODevice *createDevice() const Q_DECL_OVERRIDE;
-
-private:
-    class KZipFileEntryPrivate;
-    KZipFileEntryPrivate *const d;
 };
 
 #endif
