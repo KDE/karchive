95e4f57e579a5760ebb6ccc46efb6723e21c8fbe
Friedrich W. H. Kossebau <kossebau@kde.org>
2015-02-16 15:22:57 +0100

    When searching for first header token, only step back as much as needed
    Follow-up to REVIEW 122579

--- src/kzip.cpp
+++ src/kzip.cpp
@@ -780,9 +780,14 @@ bool KZip::openArchive(QIODevice::OpenMode mode)
                 if (buffer[0] == 'K' && buffer[1] == 3 && buffer[2] == 4) {
                     foundSignature = true;
                     dev->seek(dev->pos() - 4); // go back 4 bytes, so that the magic bytes can be found...
-                } else if (buffer[0] == 'P' || buffer[1] == 'P' || buffer[2] == 'P') {
-                    // We have another P character so we must go back a little to check if it is a magic
-                    dev->seek(dev->pos() - 3);
+                } else {
+                    for (int i = 0; i < 3; ++i) {
+                        if (buffer[i] == 'P') {
+                            // We have another P character so we must go back a little to check if it is a magic
+                            dev->seek(dev->pos() - 3 + i);
+                            break;
+                        }
+                    }
                 }
             }
         } else {
