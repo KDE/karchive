6dffddc5284502dd2ead3ee1d3774b7ade5c6c54
Dmitry Kazakov <dimula73@gmail.com>
2019-07-23 16:25:35 +0300

    Added conan tooling

--- conanfile.py
+++ conanfile.py
@@ -1,31 +1,37 @@
 from conans import ConanFile, CMake, tools
 
-class KArchiveConan(ConanFile):
-    name = "KArchive"
-    version = "5.37.0"
-    license = "LGPL-2.1"
-    url = "https://api.kde.org/frameworks/karchive/html/index.html"
-    settings = "os", "compiler", "build_type", "arch"
 
-    # build this as shared library by default, but static builds are an option
-    options = {"shared": [True, False]}
-    default_options = "shared=True"
+class KarchiveConan(ConanFile):
+    name = "karchive"
+    version = "5.61.0"
+    license = "GPLv2"
+    url = "https://api.kde.org/frameworks/karchive/html/index.html"
+    description = "Reading, creating, and manipulating file archives"
+    settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
-    exports_sources = "*"
+
+    requires = (
+        "extra-cmake-modules/[>=5.60.0]@kde/testing", # CMakeLists.txt requires 5.49.0
+        "qt/[>=5.11.0]@bincrafters/stable",
+        "zlib/1.2.11@conan/stable",
+        "bzip2/1.0.6@conan/stable",
+        "lzma/5.2.4@bincrafters/stable"
+        # TODO: QCH for documentation
+    )
+
+    scm = {
+        "type": "git",
+        "url": "auto",
+        "revision": "auto"
+    }
 
     def build(self):
         cmake = CMake(self)
-
-        # change the library install dir to just "lib" as that's what Conan expects in its packages
-        args = ['-DCMAKE_INSTALL_PREFIX="%s"' % self.package_folder,
-                '-DKDE_INSTALL_LIBDIR=lib']
-        self.run('cmake %s %s %s' % (self.source_folder, cmake.command_line, " ".join(args)))
-        self.run("cmake --build . --target install %s" % cmake.build_config)
-
-    def package(self):
-        # ideally nothing here, cmake with install takes care of it
-        pass
+        cmake.configure()
+        cmake.build()
+        cmake.install()
 
     def package_info(self):
+        self.cpp_info.resdirs = ["share"]
         self.cpp_info.libs = ["KF5Archive"]
         self.cpp_info.includedirs = ['include/KF5', 'include/KF5/KArchive']
--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,5 +1,7 @@
-from conans import ConanFile, CMake
 import os
+
+from conans import ConanFile, CMake, tools
+
 
 class KArchiveTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
@@ -7,14 +9,12 @@ class KArchiveTestConan(ConanFile):
 
     def build(self):
         cmake = CMake(self)
-        # Current dir is "test_package/build/<build_id>" and CMakeLists.txt is in "test_package"
-        cmake.configure(source_dir=self.conanfile_directory, build_dir="./")
+        # Current dir is "test_package/build/<build_id>" and CMakeLists.txt is
+        # in "test_package"
+        cmake.configure()
         cmake.build()
 
-    def imports(self):
-        self.copy("*.dll", dst="bin", src="bin")
-        self.copy("*.dylib*", dst="bin", src="lib")
-
     def test(self):
-        os.chdir("bin")
-        self.run(".%sexample" % os.sep)
+        if not tools.cross_building(self.settings):
+            os.chdir("bin")
+            self.run(".%sPackageTest" % os.sep)
