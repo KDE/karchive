feb5f185b52098310cdcdca2900821357d4a4018
Stefan Brüns <stefan.bruens@rwth-aachen.de>
2025-09-18 22:30:48 +0200

    Sanity check path length and dir nesting level early
    The nesting level and path length can be checked upfront, instead
    of doing it during the somewhat expensive directory hierarchy creation.
    In case of a malicious/malformed file, the code will bail out
    early without spending significant amount of memory and time.

--- src/karchive_p.h
+++ src/karchive_p.h
@@ -47,7 +47,7 @@ public:
 
     static QDateTime time_tToDateTime(uint seconds);
 
-    KArchiveDirectory *findOrCreate(const QString &path, int recursionCounter);
+    KArchiveDirectory *findOrCreateDirectory(const QString &path);
 
     KArchive *q = nullptr;
     KArchiveDirectory *rootDir = nullptr;
