16a108e225145dd374b9f369445d99cf36bf3c4a
Thiago Sueto <herzenschein@gmail.com>
2026-03-27 18:22:28 -0300

    QDoc KArchive

--- src/karchive.h
+++ src/karchive.h
@@ -42,22 +42,21 @@ class KARCHIVE_EXPORT KArchive
 
 protected:
     /*!
-     * Base constructor (protected since this is a pure virtual class).
+     * Constructs a new KArchive instance from \a fileName.
      *
-     * \a fileName is a local path (e.g. "/tmp/myfile.ext"),
-     * from which the archive will be read from, or into which the archive
-     * will be written, depending on the mode given to open().
+     * The \a fileName from which the archive will be read from,
+     * or into which the archive will be written,
+     * depending on the mode given to open().
      */
     explicit KArchive(const QString &fileName);
 
     /*!
-     * Base constructor (protected since this is a pure virtual class).
+     * Constructs a new KArchive instance from a QIODevice \a dev.
      *
-     * \a dev the I/O device where the archive reads its data
+     * This can be a file, but also a data buffer, a compression filter, etc.
      *
-     * Note that this can be a file, but also a data buffer, a compression filter, etc.
      * For a file in writing mode it is better to use the other constructor
-     * though, to benefit from the use of QSaveFile when saving.
+     * to benefit from the use of QSaveFile when saving.
      */
     explicit KArchive(QIODevice *dev);
 
@@ -65,11 +64,9 @@ public:
     virtual ~KArchive();
 
     /*!
-     * Opens the archive for reading or writing.
+     * Opens the archive in reading or writing \a mode.
      *
      * Inherited classes might want to reimplement openArchive instead.
-     *
-     * \a mode may be QIODevice::ReadOnly or QIODevice::WriteOnly
      * \sa close
      */
     [[nodiscard]] virtual bool open(QIODevice::OpenMode mode);
@@ -78,27 +75,23 @@ public:
      * Closes the archive.
      *
      * Inherited classes might want to reimplement closeArchive instead.
-     *
-     * Returns true if close succeeded without problems
      * \sa open
      */
     virtual bool close();
 
     /*!
-     * Returns a description of the last error
+     * Returns a description of the last error.
      * \since 5.29
      */
     QString errorString() const;
 
     /*!
-     * Checks whether the archive is open.
-     *
-     * Returns true if the archive is opened
+     * Returns whether the archive is open.
      */
     bool isOpen() const;
 
     /*!
-     * Returns the mode in which the archive was opened (QIODevice::ReadOnly or QIODevice::WriteOnly)
+     * Returns the mode in which the archive was opened.
      * \sa open()
      */
     QIODevice::OpenMode mode() const;
@@ -111,44 +104,43 @@ public:
     /*!
      * The name of the archive file, as passed to the constructor that takes a
      * fileName, or an empty string if you used the QIODevice constructor.
-     *
-     * Returns the name of the file, or QString() if unknown
      */
     QString fileName() const;
 
     /*!
+     * Returns the root directory.
+     *
      * If an archive is opened for reading, then the contents
      * of the archive can be accessed via this function.
-     * Returns the directory of the archive
+     * \code
+     * const KArchiveDirectory *dir = archive.directory();
+     * \endcode
+     * \sa KArchiveDirectory
      */
     const KArchiveDirectory *directory() const;
 
     /*!
-     * Writes a local file into the archive. The main difference with writeFile,
-     * is that this method minimizes memory usage, by not loading the whole file
-     * into memory in one go.
+     * Writes an existing local file with the given \a fileName
+     * into the archive with a new \a destName.
      *
-     * If \a fileName is a symbolic link, it will be written as is, i.e.
-     * it will not be resolved before.
+     * This method minimizes memory usage compared to writeFile
+     * by not loading the whole file into memory in one go.
      *
-     * \a fileName full path to an existing local file, to be added to the archive.
-     *
-     * \a destName the resulting name (or relative path) of the file in the archive.
+     * If \a fileName is a symbolic link, it will be written as is,
+     * it will not be resolved.
      */
     bool addLocalFile(const QString &fileName, const QString &destName);
 
     /*!
-     * Writes a local directory into the archive, including all its contents, recursively.
+     * Writes an existing local directory at the given \a path into the archive,
+     * with a new \a destName, including all its contents, recursively.
+     *
      * Calls addLocalFile for each file to be added.
      *
      * It will also add a \a path that is a symbolic link to a
-     * directory. The symbolic link will be dereferenced and the content of the
-     * directory it is pointing to added recursively. However, symbolic links
-     * *under* \a path will be stored as is.
-     *
-     * \a path full path to an existing local directory, to be added to the archive.
-     *
-     * \a destName the resulting name (or relative path) of the file in the archive.
+     * directory. The symbolic link will be dereferenced and the contents of the
+     * directory it is pointing to will be added recursively. However, symbolic links
+     * \b under \a path will be stored as is.
      */
     bool addLocalDirectory(const QString &path, const QString &destName);
 
@@ -159,19 +151,16 @@ public:
      * This method also allows some file metadata to be set.
      * However, depending on the archive type not all metadata might be regarded.
      *
-     * \a name the name of the directory
-     *
-     * \a user the user that owns the directory
-     *
-     * \a group the group that owns the directory
-     *
-     * \a perm permissions of the directory
-     *
-     * \a atime time the file was last accessed
-     *
-     * \a mtime modification time of the file
-     *
-     * \a ctime time of last status change
+     * Parameters:
+     * \list
+     * \li \a name: the name of the directory
+     * \li \a user: the user that owns the directory
+     * \li \a group: the group that owns the directory
+     * \li \a perm: permissions of the directory
+     * \li \a atime: time the file was last accessed
+     * \li \a mtime: modification time of the file
+     * \li \a ctime: time of last status change
+     * \endlist
      */
     bool writeDir(const QString &name,
                   const QString &user = QString(),
@@ -185,21 +174,17 @@ public:
      * Writes a symbolic link to the archive if supported.
      * The archive must be opened for writing.
      *
-     * \a name name of symbolic link
-     *
-     * \a target target of symbolic link
-     *
-     * \a user the user that owns the directory
-     *
-     * \a group the group that owns the directory
-     *
-     * \a perm permissions of the directory
-     *
-     * \a atime time the file was last accessed
-     *
-     * \a mtime modification time of the file
-     *
-     * \a ctime time of last status change
+     * Parameters:
+     * \list
+     * \li \a name: name of symbolic link
+     * \li \a target: target of symbolic link
+     * \li \a user: the user that owns the directory
+     * \li \a group: the group that owns the directory
+     * \li \a perm: permissions of the directory
+     * \li \a atime: time the file was last accessed
+     * \li \a mtime: modification time of the file
+     * \li \a ctime: time of last status change
+     * \endlist
      */
     bool writeSymLink(const QString &name,
                       const QString &target,
@@ -223,22 +208,17 @@ public:
      * set. However, depending on the archive type not all metadata might be
      * written out.
      *
-     * \a name the name of the file
-     *
-     * \a data the data to write
-     *
-     * \a perm permissions of the file
-     *
-     * \a user the user that owns the file
-     *
-     * \a group the group that owns the file
-     *
-     * \a atime time the file was last accessed
-     *
-     * \a mtime modification time of the file
-     *
-     * \a ctime time of last status change
-     *
+     * Parameters:
+     * \list
+     * \li \a name: the name of the file
+     * \li \a data: the data to write
+     * \li \a perm: permissions of the file
+     * \li \a user: the user that owns the file
+     * \li \a group: the group that owns the file
+     * \li \a atime: time the file was last accessed
+     * \li \a mtime: modification time of the file
+     * \li \a ctime: time of last status change
+     * \endlist
      * \since 6.0
      */
     bool writeFile(const QString &name,
@@ -251,31 +231,29 @@ public:
                    const QDateTime &ctime = QDateTime());
 
     /*!
-     * Here's another way of writing a file into an archive:
-     * Call prepareWriting(), then call writeData()
-     * as many times as wanted then call finishWriting( totalSize ).
-     * For tar.gz files, you need to know the size before hand, it is needed in the header!
-     * For zip files, size isn't used.
+     * Performs the necessary checks for writing a file into an archive.
+     *
+     * This function should be called once and is supposed to be used together
+     * with writeData and finishWriting(totalSize).
+     *
+     * For tar.gz files, you need to know the \a size beforehand.
+     * For zip files, \a size isn't used.
      *
      * This method also allows some file metadata to be
      * set. However, depending on the archive type not all metadata might be
      * regarded.
      *
-     * \a name the name of the file
-     *
-     * \a user the user that owns the file
-     *
-     * \a group the group that owns the file
-     *
-     * \a size the size of the file
-     *
-     * \a perm permissions of the file
-     *
-     * \a atime time the file was last accessed
-     *
-     * \a mtime modification time of the file
-     *
-     * \a ctime time of last status change
+     * Parameters:
+     * \list
+     * \li \a name: the name of the file
+     * \li \a user: the user that owns the file
+     * \li \a group: the group that owns the file
+     * \li \a size: the size of the file
+     * \li \a perm: permissions of the file
+     * \li \a atime: time the file was last accessed
+     * \li \a mtime: modification time of the file
+     * \li \a ctime: time of last status change
+     * \endlist
      */
     bool prepareWriting(const QString &name,
                         const QString &user,
@@ -287,37 +265,34 @@ public:
                         const QDateTime &ctime = QDateTime());
 
     /*!
-     * Write data into the current file - to be called after calling prepareWriting
+     * Writes \a data with a given chunk \a size into the current file.
      *
-     * \a data a pointer to the data
-     *
-     * \a size the size of the chunk
-     *
-     * Returns \c true if successful, \c false otherwise
+     * This function can be called multiple times
+     * and is supposed to be used together
+     * with prepareWriting and finishWriting(totalSize).
      */
     bool writeData(const char *data, qint64 size);
 
     /*!
-     * Overload for writeData(const char *, qint64);
+     * \overload writeData(const char *, qint64);
      * \since 6.0
      */
     bool writeData(QByteArrayView data);
 
     /*!
-     * Call finishWriting after writing the data.
+     * Finishes the process of writing the data using the given file \a size.
      *
-     * \a size the size of the file
+     * This function should be called once and is supposed to be used together
+     * with prepareWriting and writeData.
      * \sa prepareWriting()
      */
     bool finishWriting(qint64 size);
 
 protected:
     /*!
-     * Opens an archive for reading or writing.
+     * Opens the archive in reading or writing \a mode.
      *
      * Called by open.
-     *
-     * \a mode may be QIODevice::ReadOnly or QIODevice::WriteOnly
      */
     virtual bool openArchive(QIODevice::OpenMode mode) = 0;
 
@@ -329,46 +304,36 @@ protected:
     virtual bool closeArchive() = 0;
 
     /*!
-     * Sets error description
-     *
-     * \a errorStr error description
-     *
+     * Sets an error description \a errorStr.
      * \since 5.29
      */
     void setErrorString(const QString &errorStr);
 
     /*!
-     * Retrieves or create the root directory.
+     * Retrieves or creates the root directory if it doesn't exist.
      *
      * The default implementation assumes that openArchive() did the parsing,
      * so it creates a dummy rootdir if none was set (write mode, or no '/' in the archive).
      *
      * Reimplement this to provide parsing/listing on demand.
-     *
-     * Returns the root directory
      */
     virtual KArchiveDirectory *rootDir();
 
     /*!
-     * Write a directory to the archive.
-     * This virtual method must be implemented by subclasses.
+     * Performs the actual writing for writeDir.
      *
      * Depending on the archive type not all metadata might be used.
      *
-     * \a name the name of the directory
-     *
-     * \a user the user that owns the directory
-     *
-     * \a group the group that owns the directory
-     *
-     * \a perm permissions of the directory. Use 040755 if you don't have any other information.
-     *
-     * \a atime time the file was last accessed
-     *
-     * \a mtime modification time of the file
-     *
-     * \a ctime time of last status change
-     *
+     * Parameters:
+     * \list
+     * \li \a name: the name of the directory
+     * \li \a user: the user that owns the directory
+     * \li \a group: the group that owns the directory
+     * \li \a perm: permissions of the directory. Use 040755 if you don't have any other information.
+     * \li \a atime: time the file was last accessed
+     * \li \a mtime: modification time of the file
+     * \li \a ctime: time of last status change
+     * \endlist
      * \sa writeDir
      */
     virtual bool doWriteDir(const QString &name,
@@ -380,24 +345,18 @@ protected:
                             const QDateTime &ctime) = 0;
 
     /*!
-     * Writes a symbolic link to the archive.
-     * This virtual method must be implemented by subclasses.
+     * This performs the actual writing for writeSymLink.
      *
-     * \a name name of symbolic link
-     *
-     * \a target target of symbolic link
-     *
-     * \a user the user that owns the directory
-     *
-     * \a group the group that owns the directory
-     *
-     * \a perm permissions of the directory
-     *
-     * \a atime time the file was last accessed
-     *
-     * \a mtime modification time of the file
-     *
-     * \a ctime time of last status change
+     * \list
+     * \li \a name: name of symbolic link
+     * \li \a target: target of symbolic link
+     * \li \a user: the user that owns the directory
+     * \li \a group: the group that owns the directory
+     * \li \a perm: permissions of the directory
+     * \li \a atime: time the file was last accessed
+     * \li \a mtime: modification time of the file
+     * \li \a ctime: time of last status change
+     * \endlist
      * \sa writeSymLink
      */
     virtual bool doWriteSymLink(const QString &name,
@@ -410,26 +369,21 @@ protected:
                                 const QDateTime &ctime) = 0;
 
     /*!
-     * This virtual method must be implemented by subclasses.
+     * This performs the actual preparations for doPrepareWriting.
      *
      * Depending on the archive type not all metadata might be used.
      *
-     * \a name the name of the file
-     *
-     * \a user the user that owns the file
-     *
-     * \a group the group that owns the file
-     *
-     * \a size the size of the file
-     *
-     * \a perm permissions of the file. Use 0100644 if you don't have any more specific permissions to set.
-     *
-     * \a atime time the file was last accessed
-     *
-     * \a mtime modification time of the file
-     *
-     * \a ctime time of last status change
-     *
+     * Parameters:
+     * \list
+     * \li \a name: the name of the file
+     * \li \a user: the user that owns the file
+     * \li \a group: the group that owns the file
+     * \li \a size: the size of the file
+     * \li \a perm: permissions of the file. Use 0100644 if you don't have any more specific permissions to set.
+     * \li \a atime: time the file was last accessed
+     * \li \a mtime: modification time of the file
+     * \li \a ctime: time of last status change
+     * \endlist
      * \sa prepareWriting
      */
     virtual bool doPrepareWriting(const QString &name,
@@ -442,54 +396,41 @@ protected:
                                   const QDateTime &ctime) = 0;
 
     /*!
-     * Write data into the current file.
-     * Called by writeData.
-     *
-     * \a data a pointer to the data
-     *
-     * \a size the size of the chunk
-     *
-     * Returns \c true if successful, \c false otherwise
-     *
+     * This performs the actual writing for writeData.
+     * using a pointer to \a data with corresponding chunk \a size.
      * \sa writeData
      * \since 6.0
      */
     virtual bool doWriteData(const char *data, qint64 size);
 
     /*!
-     * Called after writing the data.
-     *
-     * This virtual method must be implemented by subclasses.
-     *
-     * \a size the size of the file
+     * This performs the actual writing for finishWriting using the file \a size.
      * \sa finishWriting()
      */
     virtual bool doFinishWriting(qint64 size) = 0;
 
     /*!
-     * Ensures that \a path exists, create otherwise.
+     * Returns \a path if it exists, creates it otherwise.
      *
-     * This handles e.g. tar files missing directory entries, like mico-2.3.0.tar.gz :)
-     *
-     * \a path the path of the directory
-     *
-     * Returns the directory with the given \a path
+     * This handles e.g. tar files missing directory entries, like mico-2.3.0.tar.gz.
      */
     KArchiveDirectory *findOrCreate(const QString &path);
 
     /*!
      * Can be reimplemented in order to change the creation of the device
-     * (when using the fileName constructor). By default this method uses
-     * QSaveFile when saving, and a simple QFile on reading.
+     * (when using the fileName constructor).
+     *
+     * By default this method uses QSaveFile when saving,
+     * and a simple QFile on reading.
      *
      * This method is called by open().
      */
     virtual bool createDevice(QIODevice::OpenMode mode);
 
     /*!
-     * Can be called by derived classes in order to set the underlying device.
+     * Can be called by derived classes in order to set the underlying device \a dev.
      *
-     * Note that KArchive will -not- own the device, it must be deleted by the derived class.
+     * Note that KArchive will \b not own the device, it must be deleted by the derived class.
      */
     void setDevice(QIODevice *dev);
 
@@ -500,6 +441,7 @@ protected:
     void setRootDir(KArchiveDirectory *rootDir);
 
 protected:
+    /*! */
     virtual void virtual_hook(int id, void *data);
 
 private:
